d5ef85e226531428f7dd384c73ea8496dd070b1e
zeoz369 <gutimar.z.a@gmail.com>
2025-07-22 17:52:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  // Función para crear el elemento subramo
-  function crearSubramo(nombre, id) {
-    const div = document.createElement("div");
-    div.classList.add("subramo");
-    div.textContent = nombre;
-    div.dataset.id = id;
-    return div;
-  }
-
-  // Mapeo id -> nombre del ramo para mostrar al abrir
+  // Mapear id de ramo a nombre para mostrar en subramo
   const cursosMap = {
     // Primer año
     "intro-matroneria": "Introducción a la Matronería y la Atención Humanizada",
     "anatomia-general": "Anatomía general",
     "biologia-celular": "Biología Celular y Molecular",
     "quimica-general": "Química General",
-    "ant
+    "antropologia-salud": "Antropología de la Salud",
+    "estrategias-vida-i": "Estrategias para la Vida Académica I",
+    "anatomia-especifica": "Anatomía Específica Gineco-obtétrica y Neonatal",
+    "microbiologia-aplicada": "Microbiología Aplicada y Parasitología",
+    "embriologia-histologia": "Embriología Humana e Histología",
+    "psicologia-general": "Psicología General en el Curso de la Vida",
+    "bioquimica": "Bioquímica (Simbología: Laboratorio)",
+    "salud-publica": "Salud Pública y Epidemiología",
+    "ingles-i": "Inglés I",
+    "estrategias-vida-ii": "Estrategias para la Vida Académica II",
+
+    // Segundo año
+    "fund-enfermeria-basica": "Fundamentos de Enfermería Básica en Gineco Obstetricia",
+    "tecnicas-enfermeria": "Técnicas de Enfermería Medico Quirúrgicas en Gineco-Obstetricia",
+    "obstetricia-salud-familiar": "Obstetricia en Salud Familiar y Comunitaria",
+    "obstetricia-fisiologica": "Obstetricia Fisiológica",
+    "psicologia-mujer": "Psicología de la Mujer",
+    "fisiologia
